b4766bb1e23746dda7cb2fd45ea5613ff352aa74
James Echmalian <ech@ech.net>
2016-01-18 10:19:31 -0500

    clean up query.js

--- crawler/query.js
+++ crawler/query.js
@@ -4,8 +4,6 @@ var App = require("./app").App;
 
 var app = new App("query");
 
-var docType = "page";
-
 app.open([ "elasticsearch" ], function(elasticsearch) {
 
   var indexName = app.args.index || "pages0";
@@ -19,33 +17,18 @@ app.open([ "elasticsearch" ], function(elasticsearch) {
     app.abort("longitude must be a number");
   }
 
-  // Load data into the index.
-  function query(callback) {
-    var location = { lon: longitude, lat: latitude };
-    var rx = 0;
-
-    (function filter() {
-      elasticsearch.geoFilter(indexName, docType, location, function(results) {
-        if (results.hits && results.hits.hits) {
-          console.log("Results", "(" + results.hits.total + ")");
-          for (var i = 0; i < results.hits.hits.length; ++i) {
-            var hit = results.hits.hits[i];
-            console.log("---");
-            console.log(" ", "url:", app.config.site.host + hit._source.uri);
-            console.log(" ", "title:", hit._source.title);
-            console.log(" ", "location:", hit._source.location);
-            console.log(" ", "score:", hit._score);
-          }
-          callback();
-        }
-        else {
-          filter();
-        }
-      });
-    })();
-  }
-
-  query(function() {
-    app.exit(0);
+  elasticsearch.geoFilter(indexName, "page", [ longitude, latitude ], function(results) {
+    if (results.hits && results.hits.hits) {
+      console.log("Results", "(" + results.hits.total + ")");
+      for (var i = 0; i < results.hits.hits.length; ++i) {
+        var hit = results.hits.hits[i];
+        console.log("---");
+        console.log(" ", "url:", app.config.site.host + hit._source.uri);
+        console.log(" ", "title:", hit._source.title);
+        console.log(" ", "location:", hit._source.location);
+        console.log(" ", "score:", hit._score);
+      }
+      app.exit(0);
+    }
   });
 });

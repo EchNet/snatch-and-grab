4c8c20d72ffb99cd012e73bfaac681f646c7566e
James Echmalian <ech@ech.net>
2016-01-23 07:52:22 -0500

    take env from ENV not command line

--- start_whwh.sh
+++ start_whwh.sh
@@ -4,17 +4,16 @@ set -e
 set -x
 
 : ${NODE:="node"}
-: ${ENV:="dev"}
 
 for CMD in $NODE; do
   command -v $CMD >/dev/null 2>&1 || { echo "$CMD required but not found.  Aborting."; exit 1; }
 done
 
 echo "Starting up crawler..."
-$NODE crawler.js --env=$ENV >>logs/crawler.log &
+$NODE crawler.js >>logs/crawler.log &
 echo "Starting up scraper..."
-$NODE scraper.js --env=$ENV >>logs/scraper.log &
+$NODE scraper.js >>logs/scraper.log &
 echo "Starting up scrape_control..."
-$NODE scrape_control.js --env=$ENV >>logs/scraper.log &
+$NODE scrape_control.js >>logs/scraper.log &
 echo "Starting up server..."
-$NODE server.js --env=$ENV >>logs/server.log &
+$NODE server.js >>logs/server.log &

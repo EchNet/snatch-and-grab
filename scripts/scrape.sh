f0300c4e3efe901ae24a87fb7d386f644b795c47
ech <ech@jechmalian-mbp.local>
2016-02-26 06:51:28 +0100

    scraper driver script, initial

---
+++ scripts/scrape.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# scrape.sh
+#
+
+set -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SITE=${1:-phony_site}
+
+[ -f ~/.whwh.env ] && source ~/.whwh.env
+
+cd $DIR/.. || exit 1
+npm install || exit 1
+s3cmd get s3://whwh/lists/$SITE.list data/$SITE.list
+node scraper --site=$SITE --in=data/$SITE.list --out=data/$SITE.data || exit 1
+s3cmd mv s3://whwh/data/$SITE.data s3://whwh/data/$SITE.data.`date +%F`
+s3cmd put data/$SITE.data s3://whwh/data/$SITE.data

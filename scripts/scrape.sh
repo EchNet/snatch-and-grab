8d0b365d49e2d54c046c5135f717a65d5da6a710
James Echmalian <ech@ech.net>
2016-03-04 15:43:52 -0500

    small mod to scrape.sh

--- scripts/scrape.sh
+++ scripts/scrape.sh
@@ -12,7 +12,7 @@ SITE=${1:-phony_site}
 
 cd $DIR/.. || exit 1
 npm install || exit 1
-s3cmd get s3://whwh/lists/$SITE.list data/$SITE.list
+s3cmd get s3://whwh/lists/$SITE.list data/$SITE.list || exit 1
 node scraper --site=$SITE --in=data/$SITE.list --out=data/$SITE.data || exit 1
 s3cmd mv s3://whwh/data/$SITE.data s3://whwh/data/$SITE.data.`date +%F`
 s3cmd put data/$SITE.data s3://whwh/data/$SITE.data

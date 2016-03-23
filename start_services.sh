4c16f563a86e00fcc8fe6afc128c6598035e1732
James Echmalian <ech@ech.net>
2016-03-23 12:49:44 -0400

    drop Redis from startup script

--- start_services.sh
+++ start_services.sh
@@ -5,15 +5,11 @@
 
 set -e
 
-: ${REDISD:="redis-server"}
 : ${ELASTICSEARCH:="elasticsearch"}
 
-for CMD in $REDISD $ELASTICSEARCH; do
+for CMD in $ELASTICSEARCH; do
   command -v $CMD >/dev/null 2>&1 || { echo "$CMD required but not found.  Aborting."; exit 1; }
 done
-
-echo "Starting Redis..."
-(cd data; $REDISD >>../logs/$REDISD.log 2>&1 ) &
 
 echo "Starting ElasticSearch..."
 $ELASTICSEARCH -d -p data/elasticsearch.pid

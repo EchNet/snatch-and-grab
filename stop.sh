dd75e98bdbb888b13433c34ce54d898628e1b222
James Echmalian <ech@ech.net>
2016-03-24 01:57:05 -0400

    drop mongodb from stop script

--- stop.sh
+++ stop.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 set -e
 
-MONGO_PIDFILE="data/mongod.lock"
 ELASTICSEARCH_PIDFILE="data/elasticsearch.pid"
 
-for PIDFILE in $MONGO_PIDFILE $ELASTICSEARCH_PIDFILE; do
+for PIDFILE in $ELASTICSEARCH_PIDFILE; do
   if [ -f $PIDFILE ]; then
     kill -3 `cat $PIDFILE` || true
   fi
 done
-
-sleep 3  # TODO: find a better way to wait for mongod to shut down 
